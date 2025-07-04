427cfb86c52e503b2d68424feb7c1be4633b8846
Mantesh96 <mantesh.hadpad@deliveryhero.in>
2025-07-04 20:03:13 +0530

    learning airthimetic operations

---
+++ learning_operations.js
@@ -0,0 +1,22 @@
+let name= "Mantesh";
+
+name= "Test1"
+//addition
+const num1=10;
+const num2=20;
+const sum= num1+num2
+//substraction
+const sub1=50;
+const sub2=30;
+
+console.log(sub1-sub2)
+// division
+const div1=50;
+const div2=25;
+console.log(div1/div2)
+//modulus
+const mod1=20;
+const mod2=10;
+console.log(mod1%mod2)
+
+
